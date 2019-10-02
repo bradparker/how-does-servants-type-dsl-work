0f89c4320919b3985208bf5a4e002035566a7884
Brad Parker <hi@bradparker.com>
2019-10-02 20:19:48 +1000

    Define the Users API as a type

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 {-# OPTIONS_GHC -Wall #-}
 
 module Main where
+
+import Servant
+  ( (:<|>)
+  , (:>)
+  , Capture
+  , Get
+  , JSON
+  )
+
+data User = User
+
+type UsersIndex =
+  Get '[JSON] [User]
+
+type UsersShow =
+  Capture "username" String
+    :> Get '[JSON] User
+
+type UsersAPI =
+  "users"
+    :> (UsersIndex :<|> UsersShow)
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
